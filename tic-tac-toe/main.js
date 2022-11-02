4515eaf96e3d491f9c62e60d07b8ef1d8f22e193
MX <dinosaurbao@pm.me>
2022-11-02 19:48:39 -0400

    made a html, css and gameboard

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,9 +1,20 @@
 var gameBoard = {
-    boards:["", "", "", "", "", "", "", "", ""]
-}
-
-var displayController={
-
+    boards:["X", null, null, null, null, "O", null, "X", "O"]
     
 }
 
+var displayController=(()=>{
+
+    const displayBoard = (board)=>{
+        const cells = document.querySelectorAll(".cell");
+        //to build js code to display or render board of boards
+        cells.forEach((cell)=>{
+
+        })
+    };
+
+    return {displayBoard};
+})();
+let board = gameBoard;
+
+displayController.displayBoard(board.boards);
