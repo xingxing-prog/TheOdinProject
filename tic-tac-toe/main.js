ef9c3375fede325e20d7f1686ae85fe2ece91b57
MX <dinosaurbao@pm.me>
2022-11-03 13:33:28 -0400

    add player and play mark

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,46 +1,73 @@
 var gameBoard={
-    boards:["X", null, null, null, "O", "X", null, null, "O"]
+    boards:["O", null, null, null, "O", "X", null, null, "O"]
 };
 
 
 var displayController=(()=>{
 
-    const gameBoard = document.querySelector(".board");
-    const displayBoard = (board)=>{
+    const board = document.querySelector(".board");
+    var value = 0;
+    const displayBoard = (boards)=>{
        
 
-        for(let item of board){
+        for(let item of boards){
             let newElement = document.createElement("div");
             newElement.setAttribute("class", "cell");
+            newElement.value = value++;
+            //newElement.setAttribute("value", value++);
             newElement.textContent = item;
-            gameBoard.appendChild(newElement);
+            newElement.addEventListener("click", player().move);
+            board.appendChild(newElement);
         }
         
     };
 
     const displayReset =()=>{
-        gameBoard.innerHTML = "";
+        board.innerHTML = "";
     }
 
     return {displayBoard, displayReset};
 })();
 
 
-const player =(mark)=>{
+const player =()=>{
 
-    const move = x=>{
-        if(gameBoard.boards[x]===null){
-            gameBoard.boards[x] = mark;
-        }
+    /*const move = (x)=>{
+        
+       if(gameBoard.boards[x] ===null){
+           gameBoard.boards[x]=mark;
+       }
+    };*/
+    let marks = document.querySelectorAll(".choice");
+    let mark = "";
+    marks.forEach((choice)=>{
+        choice.addEventListener("click", ()=>{
+            mark = choice.textContent;
+        })
+    });
+
+
+
+    const move =(e)=>{
+        
+        e.target.textContent = mark;
+        gameBoard.boards[e.target.value-9] = mark;
+        console.log(gameBoard.boards);
     }
 
     return {move};
 
 }
 
+const gamePlaying =()=>{
+
+}
+
 let board = gameBoard;
 displayController.displayBoard(board.boards);
 var player1 = player("X");
-player1.move(2);
+//player1.move(2);
+var player2 = player("O");
+//player2.move(3);
 displayController.displayReset();
 displayController.displayBoard(board.boards);
