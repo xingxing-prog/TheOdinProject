bd832a60e558cbe77454e5643c21a18ebdda041c
MX <dinosaurbao@pm.me>
2022-11-03 20:21:29 -0400

    to checkout whether the game is over because of tie or winner

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,5 +1,5 @@
 var gameBoard={
-    boards:["O", null, null, null, "O", "X", null, null, "O"]
+    boards:["X", "X", "X", null, "O", "X", null, null, "O"]
 };
 
 
@@ -14,7 +14,7 @@ var displayController=(()=>{
             let newElement = document.createElement("div");
             newElement.setAttribute("class", "cell");
             newElement.value = value++;
-            //newElement.setAttribute("value", value++);
+            
             newElement.textContent = item;
             newElement.addEventListener("click", player().move);
             board.appendChild(newElement);
@@ -25,6 +25,7 @@ var displayController=(()=>{
     const displayReset =()=>{
         board.innerHTML = "";
     }
+ 
 
     return {displayBoard, displayReset};
 })();
@@ -32,36 +33,75 @@ var displayController=(()=>{
 
 const player =()=>{
 
-    /*const move = (x)=>{
-        
-       if(gameBoard.boards[x] ===null){
-           gameBoard.boards[x]=mark;
-       }
-    };*/
     let marks = document.querySelectorAll(".choice");
     let mark = "";
     marks.forEach((choice)=>{
         choice.addEventListener("click", ()=>{
-            mark = choice.textContent;
-        })
+            mark = choice.textContent;   
+        });
+        
     });
 
-
+    let gameOver = isGameFinished;
 
     const move =(e)=>{
-        
-        e.target.textContent = mark;
-        gameBoard.boards[e.target.value-9] = mark;
-        console.log(gameBoard.boards);
+
+        if(gameOver.isWinner(gameBoard.boards)){
+            console.log("It's winner");
+        }
+
+        if(gameOver.isTie(gameBoard.boards)){
+            console.log("Game is over");
+        }
+        console.log(e.target.textContent);
+        if(gameBoard.boards[e.target.value-9] ===null || gameBoard.boards[e.target.value-9]===""){
+            e.target.textContent = mark;
+            gameBoard.boards[e.target.value-9] = mark;
+            let tie = isGameFinished.isTie(gameBoard.boards);
+            let win =isGameFinished.isWinner(gameBoard.boards);
+            console.log(win);
+            console.log(tie);
+            console.log(gameBoard.boards);
+        }
+      
     }
 
     return {move};
 
 }
 
-const gamePlaying =()=>{
 
-}
+
+const isGameFinished =(()=>{
+
+    const isTie =(board)=>{
+        for (let cell of board){
+            if(cell ==="" || cell===null){
+                return false;
+            }
+        }
+        return true;
+        
+    };
+
+    const isWinner=(board)=>{
+
+        
+
+        if((board[0]===board[1] && board[1]===board[2] && board[0]!=null && board[0] !="") || (board[3]===board[4]===board[5] &&board[3] !==null && board[3] !=="") || 
+            (board[6]===board[7]===board[8] && board[6] !==null &&board[6] !=="")){
+                return true;
+
+        }
+        else{
+            return false;
+        }
+
+
+    }
+
+    return {isTie, isWinner}
+})();
 
 let board = gameBoard;
 displayController.displayBoard(board.boards);
@@ -71,3 +111,5 @@ var player2 = player("O");
 //player2.move(3);
 displayController.displayReset();
 displayController.displayBoard(board.boards);
+console.log(isGameFinished.isTie(board.boards));
+console.log(isGameFinished.isWinner(board.boards));
