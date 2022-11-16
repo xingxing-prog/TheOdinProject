ca40c201a15b21995dbc243238e8a31247a58c4a
MX <dinosaurbao@pm.me>
2022-11-16 12:51:48 -0500

    load css

--- Restaurant-Page/src/index.js
+++ Restaurant-Page/src/index.js
@@ -1,8 +1,11 @@
+import './style.css';
 
 function component(){
     const element = document.createElement("div");
-
-    element.innerHTML = "Hello World";
+    const h1 = document.createElement("h1");
+    h1.textContent = "Authentic Chinese Food";
+    h1.setAttribute("class", "h1");
+    element.appendChild(h1);
     return element;
 }
 
